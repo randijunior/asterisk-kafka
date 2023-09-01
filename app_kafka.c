590e72228c89f68c5f16c42d91eab9a8c61aba75
randijunior <randi.junior@55pbx.com.br>
2023-09-01 08:30:30 -0300

    fix

--- app_kafka.c
+++ app_kafka.c
@@ -3,47 +3,39 @@
 #endif
 #define AST_MODULE_SELF_SYM __internal_my_module_self
 
-#include "asterisk.h"
-
-#include "asterisk/pbx.h"
-#include "asterisk/module.h"
-#include "asterisk/logger.h"
-#include "asterisk/app.h"
-
 #include <librdkafka/rdkafka.h>
 
+#include "asterisk.h"
+#include "asterisk/app.h"
+#include "asterisk/logger.h"
+#include "asterisk/module.h"
+#include "asterisk/pbx.h"
 
 static volatile sig_atomic_t run = 1;
 
 static pthread_t rk_thraed = AST_PTHREADT_NULL;
 
-static void msg_delivered_cb(rd_kafka_t *rkproducer, const rd_kafka_message_t *rkmessage, void *opaque)
-{
-    if (rkmessage->err)
-    {
-        ast_log(LOG_ERROR, "(RdKafka): Failed to delivery message: %s\n", rd_kafka_err2str(rkmessage->err));
-    }
-    else
-    {
-        char *msg = (char*) rkmessage->payload;
-        ast_log(LOG_DEBUG, "(RdKafka): Message delivery sucess (payload: %s)\n"
-                           "(partition %d" PRId32 ")\n",
+static void msg_delivered_cb(rd_kafka_t *rkproducer, const rd_kafka_message_t *rkmessage, void *opaque) {
+    if (rkmessage->err) {
+        ast_log(LOG_ERROR, "(RdKafka) Failed to delivery message: %s\n", rd_kafka_err2str(rkmessage->err));
+    } else {
+        char *msg = (char *)rkmessage->payload;
+        ast_log(LOG_DEBUG,
+                "(RdKafka) Message delivery sucess (payload: %s)\n"
+                "(partition %d" PRId32 ")\n",
                 msg, rkmessage->partition);
     }
 }
 
-static void error_cb(rd_kafka_t *rk, int err, const char *reason, void *opaque)
-{
-    ast_log(LOG_ERROR, "(RdKafka): %s\n", reason);
+static void error_cb(rd_kafka_t *rk, int err, const char *reason, void *opaque) {
+    ast_log(LOG_ERROR, "(RdKafka) Error: %s: %s\n", rd_kafka_err2name(err), reason);
 }
 
-static void logger_cb(const rd_kafka_t *r, int level, const char *fac, const char *buf)
-{
-    ast_log(LOG_NOTICE, "(RdKafka): %i %s %s\n", level, fac, buf);
+static void logger(const rd_kafka_t *r, int level, const char *fac, const char *buf) {
+    ast_log(LOG_NOTICE, "(RdKafka) %i %s %s\n", level, fac, buf);
 }
 
-static int rd_kafka_instance_init(void *data)
-{
+static int rd_kafka_instance_init(void *data) {
     rd_kafka_t **rk = data;
     rd_kafka_conf_t *conf;
     char *brokers;
@@ -53,19 +45,17 @@ static int rd_kafka_instance_init(void *data)
 
     conf = rd_kafka_conf_new();
 
-    rd_kafka_conf_set_log_cb(conf, logger_cb);
+    rd_kafka_conf_set_log_cb(conf, logger);
     rd_kafka_conf_set_dr_msg_cb(conf, msg_delivered_cb);
 
     if (rd_kafka_conf_set(conf, "bootstrap.servers", brokers, errstr,
-                          sizeof(errstr)) != RD_KAFKA_CONF_OK)
-    {
+                          sizeof(errstr)) != RD_KAFKA_CONF_OK) {
         ast_log(LOG_ERROR, "(RdKafka): %s\n", errstr);
         return -1;
     }
 
     *rk = rd_kafka_new(RD_KAFKA_PRODUCER, conf, errstr, sizeof(errstr));
-    if (!rk)
-    {
+    if (!rk) {
         ast_log(LOG_ERROR, "(RdKafka): Failed to create new producer: %s\n",
                 errstr);
         return -1;
@@ -73,27 +63,22 @@ static int rd_kafka_instance_init(void *data)
     return 0;
 }
 
-static void rd_kafka_instance_destroy(void *data)
-{
+static void rd_kafka_instance_destroy(void *data) {
     rd_kafka_t **rk = data;
 
-    rd_kafka_flush(*rk, 10 * 2000);
+    rd_kafka_flush(*rk, 10 * 1000);
     rd_kafka_destroy(*rk);
 }
 
-static struct rk_producer *create_producer() {}
-
 AST_THREADSTORAGE_CUSTOM(producer_instance, rd_kafka_instance_init, rd_kafka_instance_destroy);
 
-static int kafka_producer_exec(struct ast_channel *chan, const char *vargs)
-{
+static int kafka_producer_exec(struct ast_channel *chan, const char *vargs) {
     char *data;
     rd_kafka_t **rk;
     char errstr[512];
     rd_kafka_resp_err_t err;
 
-    if (!(rk = ast_threadstorage_get(&producer_instance, sizeof(*rk))))
-    {
+    if (!(rk = ast_threadstorage_get(&producer_instance, sizeof(*rk)))) {
         ast_log(LOG_ERROR, "Cannot alocate producer structure\n");
         return -1;
     }
@@ -104,8 +89,7 @@ static int kafka_producer_exec(struct ast_channel *chan, const char *vargs)
 
     data = ast_strdupa(vargs);
 
-    if (ast_strlen_zero(data))
-    {
+    if (ast_strlen_zero(data)) {
         ast_log(LOG_WARNING, "%s requires an argument (topic,message)\n", "ProduceToKafka");
         return -1;
     }
@@ -113,31 +97,26 @@ static int kafka_producer_exec(struct ast_channel *chan, const char *vargs)
 
     ast_log(LOG_NOTICE, "should send message: %s, to topic: %s", args.msg, args.topic);
 
-    if (chan)
-    {
+    if (chan) {
         ast_autoservice_start(chan);
     }
 
-    err = rd_kafka_producev(*rk, 
-                            RD_KAFKA_V_TOPIC("quickstart-events"), 
-                            RD_KAFKA_V_MSGFLAGS(RD_KAFKA_MSG_F_COPY), 
-                            RD_KAFKA_V_VALUE("a", 1), 
-                            RD_KAFKA_V_OPAQUE(NULL), 
+    err = rd_kafka_producev(*rk,
+                            RD_KAFKA_V_TOPIC("quickstart-events"),
+                            RD_KAFKA_V_MSGFLAGS(RD_KAFKA_MSG_F_COPY),
+                            RD_KAFKA_V_VALUE("a", 1),
+                            RD_KAFKA_V_OPAQUE(NULL),
                             RD_KAFKA_V_END);
 
-    if (err)
-    {
+    if (err) {
         ast_log(LOG_ERROR, "FAILED TO DELIVERY MESSAGE %s\n", rd_kafka_err2str(err));
-    }
-    else
-    {
+    } else {
         ast_log(LOG_WARNING, "Enqueued message");
     }
 
     rd_kafka_poll(*rk, 0);
 
-    if (chan)
-    {
+    if (chan) {
         ast_autoservice_stop(chan);
     }
 
@@ -146,27 +125,25 @@ static int kafka_producer_exec(struct ast_channel *chan, const char *vargs)
 
 static void *rk_poll(void *data) {
     rd_kafka_t **rk;
-    if (!(rk = ast_threadstorage_get(&producer_instance, sizeof(*rk))))
-    {
+    if (!(rk = ast_threadstorage_get(&producer_instance, sizeof(*rk)))) {
         ast_log(LOG_ERROR, "Cannot get producer structure\n");
         return NULL;
     }
     while (run) {
-    if(rd_kafka_outq_len(*rk) > 0) {
-        ast_log(LOG_DEBUG, "POOLING");
-        rd_kafka_poll(*rk, 10 * 1000); 
+        if (rd_kafka_outq_len(*rk) > 0) {
+            ast_log(LOG_DEBUG, "POOLING");
+            rd_kafka_poll(*rk, 10 * 1000);
+        }
     }
-}
-return NULL;
+    return NULL;
 }
 
-int unload_module(void)
-{
+int unload_module(void) {
     int res = 0;
 
     run = 0;
 
-    if(rk_thraed != AST_PTHREADT_NULL) {
+    if (rk_thraed != AST_PTHREADT_NULL) {
         pthread_kill(rk_thraed, SIGURG);
         pthread_join(rk_thraed, NULL);
     }
@@ -176,18 +153,15 @@ int unload_module(void)
     return res;
 }
 
-int load_module(void)
-{
+int load_module(void) {
     int res = 0;
 
-    if(ast_pthread_create_background(&rk_thraed, NULL, rk_poll, NULL) > 0) {
+    if (ast_pthread_create_background(&rk_thraed, NULL, rk_poll, NULL) > 0) {
         ast_log(LOG_ERROR, "FAILED TO CREATE THREAD");
-        unload_module();
         return AST_MODULE_LOAD_DECLINE;
     }
 
     res |= ast_register_application("ProduceToKafka", kafka_producer_exec, "todo", "todo");
-
 
     return res;
 }
